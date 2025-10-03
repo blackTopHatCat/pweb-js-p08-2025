5b7f4a99721c4a30cf17ece19387235cfa8ad472
instaboy <>
2025-10-03 20:05:21 +0700

    before pushing to github

--- external.js
+++ external.js
@@ -1,26 +1,42 @@
-const form = document.getElementById("submit");
-var response;
+let response;
+let current_page = window.location.href.replace(/^.*[\\\/]/, "");
 
-form.addEventListener("click", (event) => {
-  event.preventDefault();
-  let input_username = document.getElementById("username").value;
-  let input_password = document.getElementById("password").value;
+if (current_page == "login.html") {
+  const form = document.getElementById("submit");
+  form.addEventListener("click", (event) => {
+    document.getElementById("login_feedback").innerHTML = "Sending..";
+    event.preventDefault();
+    let input_username = document.getElementById("username").value;
+    let input_password = document.getElementById("password").value;
 
-  fetch("https://dummyjson.com/user/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      username: input_username,
-      password: input_password,
-      expiresInMins: 30, // optional, defaults to 60
-    }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      response = data;
-      localStorage.setItem("response", JSON.stringify(response))
+    fetch("https://dummyjson.com/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: input_username,
+        password: input_password,
+        expiresInMins: 30, // optional, defaults to 60
+      }),
     })
-    .then(() => {
-      console.log(response);
-    });
-});
+      .then((res) => res.json())
+      .then((data) => {
+        response = data;
+        localStorage.setItem("response", JSON.stringify(response));
+
+        if (
+          response.message == "Invalid credentials" ||
+          input_username == "" ||
+          input_password == ""
+        ) {
+          document.getElementById("login_feedback").innerHTML =
+            "Username or password is invalid!";
+        } else {
+          document.getElementById("login_feedback").innerHTML = "Success!";
+          window.location.href = 'shop.html'; 
+        }
+      })
+      .then(() => {
+        console.log();
+      });
+  });
+}
